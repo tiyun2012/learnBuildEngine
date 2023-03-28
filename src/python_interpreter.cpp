d22bfa9ce258517e3e1b3a0093f9aecd2bc6559f
Nhat <hoathinhvietnam2016@gmail.com>
2023-03-29 05:20:00 +0700

    fix bug Shift+ back arrow cause crash

--- src/python_interpreter.cpp
+++ src/python_interpreter.cpp
@@ -58,66 +58,75 @@ PyObject* PythonInterpreter::py_write(PyObject* self, PyObject* args) {
     python_output.append(str);
     Py_RETURN_NONE;
 }
+
+static int InputTextCallback(ImGuiInputTextCallbackData* data) {
+    if (data->HasSelection() && data->SelectionStart >= 0 && data->SelectionEnd <= data->BufTextLen) {
+        if (data->SelectionStart < data->SelectionEnd) {
+            std::string& selected_text = *static_cast<std::string*>(data->UserData);
+            selected_text.assign(data->Buf + data->SelectionStart, data->Buf + data->SelectionEnd);
+        }
+    }
+    return 0;
+}
+
+
+
+
+    //--------------------
 void PythonInterpreter::draw() {
-    ImGui::Begin("Python Editor");
-    // Set a size for the multi-line text editor
+    ImGui::Begin("Python Interpreter");
+
     ImVec2 input_text_size = ImVec2(0, ImGui::GetTextLineHeight() * 8);
 
-    // bool enter_pressed = ImGui::InputText("##input", input_buffer, IM_ARRAYSIZE(input_buffer), ImGuiInputTextFlags_EnterReturnsTrue); // olf code for single line input
-    
-    // Use ImGui::InputTextMultiline instead of ImGui::InputText
-    bool enter_pressed = ImGui::InputTextMultiline("##input", input_buffer, IM_ARRAYSIZE(input_buffer), input_text_size, ImGuiInputTextFlags_EnterReturnsTrue);
+    std::string selected_text;
+    ImGui::InputTextMultiline("##input", input_buffer, IM_ARRAYSIZE(input_buffer), input_text_size, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackAlways, InputTextCallback, (void*)&selected_text);
+
+    bool enter_pressed = ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Enter)) && ImGui::IsWindowFocused();
 
     ImGui::SameLine();
 
     // Add a separate button for executing Python code
     bool execute_button_pressed = ImGui::Button("Execute");
 
+
     if (enter_pressed || execute_button_pressed) {
-        if (strlen(input_buffer) > 0) 
-        {
-            // Execute Python code
-            PyObject* result = PyRun_String(input_buffer, Py_file_input, globals, locals);// Py_single_input
+        // Execute the selected Python code or the entire code if there's no selection
+        const char* code_to_execute = selected_text.empty() ? input_buffer : selected_text.c_str();
+        PyObject* result = PyRun_String(code_to_execute, Py_file_input, globals, locals);
 
-    
+        if (result) {
+            if (result != Py_None) {
+                PyObject* result_str = PyObject_Repr(result);
+                const char* result_cstr = PyUnicode_AsUTF8(result_str);
+                output_buffer.append(result_cstr);
+                output_buffer.append("\n");
+                Py_DECREF(result_str);
+            }
+            Py_DECREF(result);
+        } else {
+            if (PyErr_Occurred()) {
+                PyObject* exc_type;
+                PyObject* exc_value;
+                PyObject* exc_traceback;
+                PyErr_Fetch(&exc_type, &exc_value, &exc_traceback);
+                PyErr_NormalizeException(&exc_type, &exc_value, &exc_traceback);
 
-            if (result) 
-            {
-                if (result != Py_None) 
-                { // Add this line
-                    PyObject* result_str = PyObject_Repr(result);
-                    const char* result_cstr = PyUnicode_AsUTF8(result_str);
-                    output_buffer.append(result_cstr);
-                    output_buffer.append("\n");
-                    Py_DECREF(result_str);
-                }
-                Py_DECREF(result);
-            } 
-            else 
-            {
-                if (PyErr_Occurred()) 
-                {
-                    PyObject* exc_type;
-                    PyObject* exc_value;
-                    PyObject* exc_traceback;
-                    PyErr_Fetch(&exc_type, &exc_value, &exc_traceback);
-                    PyErr_NormalizeException(&exc_type, &exc_value, &exc_traceback);
-
-                    PyObject* exc_str = PyObject_Repr(exc_value);
-                    const char* exc_cstr = PyUnicode_AsUTF8(exc_str);
-                    output_buffer.append("Error: ");
-                    output_buffer.append(exc_cstr);
-                    output_buffer.append("\n");
-                    Py_DECREF(exc_type);
-                    Py_DECREF(exc_value);
-                    Py_DECREF(exc_traceback);
-                    Py_DECREF(exc_str);
-                }
+                PyObject* exc_str = PyObject_Repr(exc_value);
+                const char* exc_cstr = PyUnicode_AsUTF8(exc_str);
+                output_buffer.append("Error: ");
+                output_buffer.append(exc_cstr);
+                output_buffer.append("\n");
+                Py_DECREF(exc_type);
+                Py_DECREF(exc_value);
+                Py_DECREF(exc_traceback);
+                Py_DECREF(exc_str);
             }
         }
+
         PyErr_Clear();
         memset(input_buffer, 0, sizeof(input_buffer));
     }
+
 
     ImGui::SameLine();
     if (ImGui::Button("Clear")) {
@@ -133,6 +142,6 @@ void PythonInterpreter::draw() {
     ImGui::TextUnformatted(output_buffer.begin(), output_buffer.end());
     ImGui::EndChild();
 
+
     ImGui::End();
 }
-
